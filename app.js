9ea0c92a6c401771966fc93ab78ce7ace34e4451
paulo.silva <pharaujo14@gmail.com>
2020-07-12 18:48:36 -0300

    ...

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://sheltered-thicket-50121.herokuapp.com/',
+    origin: 'https://sheltered-thicket-50121.herokuapp.com',
   })
 );
 
